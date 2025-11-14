0a40956762411440f5b0f73c25b60c9ff4ada643
Ariel Villanea <ariel-villanea@users.noreply.github.com>
2025-11-13 23:18:42 -0600

    Fixing baseUrl issue now that it's using a custom domain.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: 'https://ariel-villanea.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ariel-villanea/',
+  baseUrl: '/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
